b5d74be855bfca99d278e7afd11c9d4f46c0fbbe
pamuba <me.in@live.in>
2024-08-26 20:01:05 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyApp
+{
+    class Person {
+        public int id;
+        public string FistName;
+        public string LastName;
+
+        public override string ToString()
+        {
+            return this.FistName+" "+this.LastName;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //var names = new string[] { "each" ,"element","explicitly"};
+            //Array.Sort(names);
+            //Array.Reverse(names);
+            //string newval = string.Join(",\n", names);
+            //Console.WriteLine(newval);
+        }
+    }
+}
+
+
+
---
+++ ConsoleApp2/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Dim i As Integer
+        i = 0
+        Console.WriteLine(GetType(i))
+    End Sub
+End Module
